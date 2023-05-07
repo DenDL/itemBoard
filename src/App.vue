f3dbb3b7127dbb7dbb27a6bc7d22ff4e884cea97
denis <ddenisenko1989@gmail.com>
2023-05-08 01:23:26 +0300

    Art-2 - Adaptive

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,27 @@
 <template>
     <div class="container mx-auto">
-        <div class="flex">
-            <div class="w-1/4 pt-[6rem] pr-[4rem]">
-                <listItems :listItems="task2.data" @addItem="addItem"/>
-            </div>
-            <div class="w-3/4">
+        <div class="flex mb-8">
+            <div class="w-0 lg:w-1/4" />
+            <div class="w-full lg:w-3/4">
                 <currencyButtonList :listCurrency="createListCurrency()"
                                     :activeCurrency="activeCurrency"
                                     @clickCurrency="changeActiveCurrency"/>
-
-                <div class="flex column-2 pt-[2.35rem]">
-                    <cardItem :addClass="['sm:mr-2']"
+            </div>
+        </div>
+        <div class="flex flex-col lg:flex-row">
+            <div class="w-full lg:w-1/4 lg:pr-[2rem] mb-4">
+                <listItems :listItems="task2.data" @addItem="addItem"/>
+            </div>
+            <div class="w-full lg:w-3/4">
+                <div class="flex flex-col lg:flex-row lg:column-2">
+                    <cardItem :addClass="['lg:mr-2']"
                               cardTitle="Last Checked Item"
                               :tableDescription="htmlDescription(activeListItem[activeListItem.length - 1])"
                               :totalAmount="convertCurrencyFull(activeListItem[activeListItem.length - 1]) ? convertCurrencyFull(activeListItem[activeListItem.length - 1]) + ' ' + activeCurrency : null"/>
-                    <cardItem :addClass="['sm:ml-2']"
+                    <cardItem :addClass="['lg:ml-2']"
                               cardTitle="Total"
                               :tableDescription="htmlDescription(activeListItem)"
-                              :totalAmount="convertCurrencyFull(activeListItem) ? convertCurrencyFull(activeListItem) + ' ' + activeCurrency : null"/>
+                              :totalAmount="convertCurrencyFull(activeListItem) ? `<span class='inline-block truncate w-10/12 text-base max-w-prose'>`+convertCurrencyFull(activeListItem) + '</span> ' + activeCurrency : null"/>
                 </div>
             </div>
         </div>
@@ -102,11 +106,13 @@ export default {
             return bodyHtml;
 
         },
-        convertCurrency(item, boolCurrency = false) {
+        convertCurrency(item, boolCurrency = false, number = false) {
             if (item.currency === this.activeCurrency) {
                 return item.price + (boolCurrency ? " " + item.currency : '');
+            } else if (number) {
+                return item.price * this.task2['currencies-pairs'][item.currency+'-'+this.activeCurrency];
             } else {
-                return item.price * this.task2['currencies-pairs'][item.currency+'-'+this.activeCurrency] + (boolCurrency ? " " + this.activeCurrency : '');
+                return '<span class="inline-block truncate w-10/12 text-base max-w-prose">' + item.price * this.task2['currencies-pairs'][item.currency+'-'+this.activeCurrency] + '</span>' + (boolCurrency ? " " + this.activeCurrency : '');
             }
         },
         convertCurrencyFull(items) {
@@ -115,7 +121,7 @@ export default {
 
             if(itemsLoc && itemsLoc.length >= 1) {
                 itemsLoc.forEach(item => {
-                    fullPrice += +this.convertCurrency(item);
+                    fullPrice += +this.convertCurrency(item, false, true);
                 });
             }
 
--- src/components/cardItem.vue
+++ src/components/cardItem.vue
@@ -13,7 +13,7 @@ export default defineComponent({
 </script>
 
 <template>
-    <div class="flex flex-col w-1/2 border rounded-lg border-slate-500 bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700"
+    <div class="flex flex-col lg:w-1/2 border rounded-lg border-slate-500 bg-white p-6 dark:bg-neutral-700 mb-2"
          :class="addClass">
         <h5 class="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
             {{ cardTitle }}
@@ -24,12 +24,12 @@ export default defineComponent({
         <p v-else
            class="mb-auto pt-2 pb-4">No checked item</p>
         <div class="flex justify-between columns-2">
-            <div class="md:w-1/2">
+            <div class="w-0 lg:w-1/3">
 
             </div>
-            <div class="md:w-1/2 flex items-center columns-2">
+            <div class="w-full lg:w-2/3 flex items-center columns-2">
                 <div class="text-2xl text-cyan-600">Summary:</div>
-                <div class="px-[2rem]">{{ totalAmount || '-' }}</div>
+                <div class="px-[2rem]" v-html="totalAmount"></div>
             </div>
         </div>
     </div>
